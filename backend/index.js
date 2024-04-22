eddaf321458007d0501ce1309f3066909a3046bb
Ameer Hamza <ameer.hamza102002@gmail.com>
2024-04-22 22:10:38 +0500

    Server initiated

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const express=require("express");
+const app=express();
+const dotenv=require("dotenv");
+dotenv.config();
+app.use(express.json());
+
+app.listen(process.env.PORT,()=>{
+  console.log(`Server is listening at port ${process.env.PORT}`);
+})
